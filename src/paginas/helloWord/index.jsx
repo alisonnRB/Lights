ea06ba6bd638f1ba88b4843f067ebc61c6f54e9d
alisonnRB <alisonrbjs@gmail.com>
2023-12-16 19:54:46 -0300

    styles init

---
+++ src/paginas/helloWord/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div className="home">
+            <p className="lets">Let's say hello to the world together?</p>
+
+            <div className="botao">
+                <span className="braille"></span>
+                <p>HELLO WORLD</p>
+                <p>click here and let's go</p>
+            </div>
+
+            <p className="apresentation">My name is Álison, nice to meet you!</p>
+        </div>
+    );
+}
