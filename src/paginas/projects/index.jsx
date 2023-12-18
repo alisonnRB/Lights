0aac68e62e54e38042cf20b523723e2274217435
alisonnRB <alisonrbjs@gmail.com>
2023-12-18 19:58:21 -0300

    card costume

--- src/paginas/projects/index.jsx
+++ src/paginas/projects/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import '../../output/style_project.css';
 
 import Card_project from "./card_project";
@@ -7,12 +8,13 @@ import ihm from '../../drawble/IHM.png';
 import palabraria from '../../drawble/Palabraria.png';
 
 export default function Projects() {
+    const navigate = useNavigate();
     const [open, setOpen] = useState(false);
     const [openContent, setOpenContent] = useState(false)
     const [turn, setTurn] = useState(false);
     const list = {
         0: { 'img': ihm, 'bg': 'white', 'name': 'IHM', 'ref': 'https:\\www.literary-ihm.com', 'ready': true, 'text': 'IHM é uma rede social literateraria, que permite escevrever e ler livros assim como permite a interação entre os usuarios.' },
-        1: { 'img': palabraria, 'bg': 'black', 'name': 'Palabraria', 'ref' : '', 'ready': false, 'text': 'Um dicionário ilustrado e livre de lingua espanhola com onjetivo de ampliar o repertório da lingua de uma maneira cooperativa com as instituições educacionais' },
+        1: { 'img': palabraria, 'bg': 'black', 'name': 'Palabraria', 'ref': '', 'ready': false, 'text': 'Um dicionário ilustrado e livre de lingua espanhola com onjetivo de ampliar o repertório da lingua de uma maneira cooperativa com as instituições educacionais' },
     }
 
     useEffect(() => {
@@ -43,6 +45,16 @@ export default function Projects() {
         return itens;
     }
 
+    const Navigate_menu = () => {
+        setTurn(true);
+        setOpen(false);
+        
+        const timeoutId = setTimeout(() => {
+            navigate('/menu')
+        }, 1000);
+        return () => clearTimeout(timeoutId);
+    }
+
     return (
         <div className="projects">
             {!open ? <div className={`light ${turn ? 'off' : null}`}></div> : null}
@@ -51,11 +63,15 @@ export default function Projects() {
                 <p>My Projects</p>
             </div>
 
-            { openContent ? <div className="content-cards">
+            {openContent ? <div className="content-cards">
                 {gera_cards()}
 
                 <div className="coming">New projects coming soon . . .</div>
             </div> : null}
+
+            <div className="back-card">
+                <p onClick={() => { Navigate_menu() }}>Return</p>
+            </div>
 
         </div>
     )
