5f2f68446fa22c4bfb607a4554082a9439a4648f
alisonnRB <alisonrbjs@gmail.com>
2024-03-27 16:51:24 -0300

    atualização

--- src/paginas/projects/index.jsx
+++ src/paginas/projects/index.jsx
@@ -9,6 +9,7 @@ import Card_project from "./card_project";
 
 import ihm from '../../drawble/IHM.png';
 import palabraria from '../../drawble/Palabraria.png';
+import lines from '../../drawble/lines.png';
 
 export default function Projects() {
     const { language } = useContext(LanguageContext);
@@ -24,8 +25,8 @@ export default function Projects() {
             'ref': 'https://www.literary-ihm.com',
             'git': 'https://github.com/alisonnRB/IHM.git',
             'ready': true,
-            'activity': 'O IHM foi um projeto extenso e denso, realizado com o objetivo de aprimorar minhas habilidades no desenvolvimento web.',
-            'activity_en' : 'IHM was an extensive and dense project, carried out with the aim of improving my web development skills.',
+            'activity': 'O IHM foi um projeto extenso e denso, realizado com React e php com inituito de aprimorar minhas habilidades.',
+            'activity_en' : 'The IHM was an extensive and dense project, carried out with React and PHP with the aim of improving my skills.',
             'text': 'IHM é uma rede social literária que permite escrever e ler livros, além de possibilitar a interação entre os usuários.',
             'text_en' : 'IHM is a literary social network that allows you to write and read books, as well as enabling interaction between users.'
         },
@@ -36,9 +37,24 @@ export default function Projects() {
             'name': 'Palabraria',
             'ref': '',
             'git': 'https://github.com/alisonnRB/new-palabraria',
-            'ready': false,
+            'ready': true,
             'activity': 'O projeto Palabraria foi realizado por meio de uma bolsa promovida pelo IFRS, com o objetivo de aprimorar a qualidade do ensino da língua espanhola. Com um servidor com paradgma orientado a objetos.',
-            'text': 'Um dicionário ilustrado e gratuito da língua espanhola, com o objetivo de ampliar o repertório linguístico de maneira colaborativa com as instituições educacionais.'
+            'activity_en': 'The Palabraria project was carried out through a grant promoted by IFRS, with the aim of improving the quality of Spanish language teaching. With a server with an object-oriented paradigm',
+            'text': 'Um dicionário ilustrado e gratuito da língua espanhola, com o objetivo de ampliar o repertório linguístico de maneira colaborativa com as instituições educacionais.',
+            'text_en': 'A free illustrated dictionary of the Spanish language, with the aim of expanding the linguistic repertoire in a collaborative way with educational institutions.'
+        },
+
+        2: {
+            'img': lines,
+            'bg': '#f1f1f1',
+            'name': 'Analist-color',
+            'ref': 'Analist-color.vercel.app',
+            'git': 'https://github.com/alisonnRB/Interface-analist-color',
+            'ready': true,
+            'activity': 'O projeto Analsit-color é na verdade um interface para interagir com a api criada por mim, sendo uma ferramenta para designs mais personalizados.',
+            'activity_en': 'The Analsit-color project is actually an interface to interact with the api created by me, being a tool for more personalized designs.',
+            'text': 'A interface foi construída em Vue.js e interage com a api criada em laravel mostrando as principais caracteristicas de cor em uma imagem.',
+            'text_en': 'The interface was built in Vue.js and interacts with the api created in Laravel, showing the main color characteristics in an image.'
         },
     }
 
